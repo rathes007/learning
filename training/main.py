59097431ae528e56e5a7400f1df77eef17c28a69
rathes007 <mailramratheesh47@gmail.com>
2022-03-25 02:38:57 +0530

    characters

---
+++ training/main.py
@@ -0,0 +1,8 @@
+name = "ram"
+
+if len(name) < 3:
+    print("name must be atleast 3 characters")
+elif len(name) > 50:
+    print("name can be maximum 50 characters")
+else:
+    print("your name looks good")
