9d67ea8eaff724abfd4393cda281b9939e855dfa
rathes007 <mailramratheesh47@gmail.com>
2022-03-25 22:28:20 +0530

    added extra print

--- training/main.py
+++ training/main.py
@@ -1,6 +1,16 @@
-i = 1
-while i <= 5:
-    print(i)
-    i += 1
-print("done")
+is_hot = False
+is_cold = False
 
+if is_hot:
+    print("it's a hot day")
+    print("drink plenty of water")
+
+elif is_cold:
+    print("it's a cold day")
+    print("wear warm cloths")
+
+else:
+    print("it's a lovely day")
+
+
+print('x')
