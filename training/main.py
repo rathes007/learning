ed56d938502668e879b6d4b62f110dbead1c37e0
rathes007 <mailramratheesh47@gmail.com>
2022-05-24 19:57:49 +0530

    adam training

--- training/main.py
+++ training/main.py
@@ -1,8 +1,34 @@
-name = "ram"
+def fonts(doc, granularity=False):
+    """Extracts fonts and their usage in PDF documents.
+    :param doc: PDF document to iterate through
+    :type doc: <class 'fitz.fitz.Document'>
+    :param granularity: also use 'font', 'flags' and 'color' to discriminate text
+    :type granularity: bool
+    :rtype: [(font_size, count), (font_size, count}], dict
+    :return: most used fonts sorted by count, font style information
+    """
+    styles = {}
+    font_counts = {}
 
-if len(name) < 3:
-    print("name must be atleast 3 characters")
-elif len(name) > 50:
-    print("name can be maximum 50 characters")
-else:
-    print("your name looks good")
+    for page in doc:
+        blocks = page.getText("dict")["blocks"]
+        for b in blocks:  # iterate through the text blocks
+            if b['type'] == 0:  # block contains text
+                for l in b["lines"]:  # iterate through the text lines
+                    for s in l["spans"]:  # iterate through the text spans
+                        if granularity:
+                            identifier = "{0}_{1}_{2}_{3}".format(s['size'], s['flags'], s['font'], s['color'])
+                            styles[identifier] = {'size': s['size'], 'flags': s['flags'], 'font': s['font'],
+                                                  'color': s['color']}
+                        else:
+                            identifier = "{0}".format(s['size'])
+                            styles[identifier] = {'size': s['size'], 'font': s['font']}
+
+                        font_counts[identifier] = font_counts.get(identifier, 0) + 1  # count the fonts usage
+
+    font_counts = sorted(font_counts.items(), key=itemgetter(1), reverse=True)
+    if len(font_counts) < 1:
+        raise ValueError("Zero discriminating fonts found!")
+
+    return font_counts, styles
+    print(page)
